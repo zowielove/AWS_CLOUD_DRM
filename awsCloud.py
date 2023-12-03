741d0d01483166fefea4d09b15fea00af16f3837
Kyd0312 <71116531+zowielove@users.noreply.github.com>
2023-12-04 01:02:05 +0900

    Update
    get Access ID, Access Key, Region Name as running parameter.
    cloud initialization function added.

--- awsCloud.py
+++ awsCloud.py
@@ -1,3 +1,43 @@
+import sys
+import boto3
+
+ec2, ssm, cloudwatch, sts = None, None, None, None
+accessID, accessKey = None, None
+regionName = 'eu-north-1'
+
+
+def checkKey():
+    global accessID, accessKey, regionName
+
+    if len(sys.argv) < 2:
+        print('run "python awsCloud.py (Access ID - required) (Access Key - required) (Region - optional)"')
+        exit(-1)
+    elif len(sys.argv) == 2:
+        accessID = sys.argv[1]
+        accessKey = sys.argv[2]
+
+    elif len(sys.argv) == 3:
+        accessID = sys.argv[1]
+        accessKey = sys.argv[2]
+        regionName = sys.argv[3]
+
+    print(f"Access ID: {accessID}")
+    print(f"Access Key: {accessKey}")
+    print(f"Region Name: {regionName}")
+
+
+def initCloud():
+    global ec2, ssm, cloudwatch, sts
+
+    ec2 = boto3.client('ec2', regionName=regionName, accessID=accessID,
+                       accessKey=accessKey)
+    ssm = boto3.client('ssm', regionName=regionName, accessID=accessID,
+                       accessKey=accessKey)
+    cloudwatch = boto3.client('cloudwatch', regionName=regionName, accessID=accessID,
+                              accessKey=accessKey)
+    sts = boto3.client('sts', accessID=accessID, accessKey=accessKey)
+
+
 def printMenu():
     print("------------------------------------------------------------")
     print("  1. list instance                2. available zones        ")
