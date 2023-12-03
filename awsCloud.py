f8994f0aeb311e225f19e9dc6d3b55b902aabdee
Kyd0312 <71116531+zowielove@users.noreply.github.com>
2023-12-04 02:21:43 +0900

    Update
    read ID-Key from file function added.

--- awsCloud.py
+++ awsCloud.py
@@ -10,17 +10,38 @@ regionName = 'eu-north-1'
 def checkKey():
     global accessID, accessKey, regionName
 
-    if len(sys.argv) < 2:
-        print('run "python awsCloud.py (Access ID - required) (Access Key - required) (Region - optional)"')
-        exit(-1)
-    elif len(sys.argv) == 2:
-        accessID = sys.argv[1]
-        accessKey = sys.argv[2]
+    if len(sys.argv) == 3 and sys.argv[1] == '-f':
+        file = open(sys.argv[2], 'r')
 
-    elif len(sys.argv) == 3:
-        accessID = sys.argv[1]
-        accessKey = sys.argv[2]
-        regionName = sys.argv[3]
+        line = file.readline().strip()
+        aID = line.split('=')[1]
+        if aID != '':
+            accessID = aID
+        else:
+            print("Error: accessID is empty.")
+            exit(-1)
+
+        line = file.readline().strip()
+        aKey = line.split('=')[1]
+        if aKey != '':
+            accessKey = aKey
+        else:
+            print("Error: accessKey is empty.")
+            exit(-1)
+
+    elif len(sys.argv) == 4:
+        accessID = sys.argv[2]
+        accessKey = sys.argv[3]
+
+    elif len(sys.argv) == 5:
+        accessID = sys.argv[2]
+        accessKey = sys.argv[3]
+        regionName = sys.argv[4]
+    else:
+        print('>> [ Usage ]')
+        print('python awsCloud.py -f (File Name) ')
+        print('python awsCloud.py -m (Access ID - required) (Access Key - required) (Region - optional)')
+        exit(-1)
 
     print(">> [ Initial Information ]")
     print(f"\tAccess ID: {accessID}")
@@ -32,13 +53,13 @@ def initCloud():
     global ec2, ssm, cloudwatch, sts
 
     try:
-        ec2 = boto3.client('ec2', regionName=regionName, accessID=accessID,
-                           accessKey=accessKey)
-        ssm = boto3.client('ssm', regionName=regionName, accessID=accessID,
-                           accessKey=accessKey)
-        cloudwatch = boto3.client('cloudwatch', regionName=regionName, accessID=accessID,
-                                  accessKey=accessKey)
-        sts = boto3.client('sts', accessID=accessID, accessKey=accessKey)
+        ec2 = boto3.client('ec2', region_name=regionName, aws_access_key_id=accessID,
+                           aws_secret_access_key=accessKey)
+        ssm = boto3.client('ssm', region_name=regionName, aws_access_key_id=accessID,
+                           aws_secret_access_key=accessKey)
+        cloudwatch = boto3.client('cloudwatch', region_name=regionName, aws_access_key_id=accessID,
+                                  aws_secret_access_key=accessKey)
+        sts = boto3.client('sts', aws_access_key_id=accessID, aws_secret_access_key=accessKey)
     except Exception as e:
         print(f"Error: {e}.")
         exit(-1)
